d98695480ae7062dd07c00037b58ffa19cecc15d
Atticus Pomerantz <rapomerantz@gmail.com>
2018-06-21 16:13:05 -0500

    for loop

--- newBoston1.py
+++ newBoston1.py
@@ -1,6 +1,27 @@
 
 
 
+
+
+
+
+
+#For Loops
+'''
+foods = ['taco', 'burrito', 'pasta', 'coffee']
+
+#functions in the same way as a JS 'for in' loop
+#use braket notation like [1:3] to only to through a slice of the list
+#a for loop of a string will loop through each character in a string
+
+for item in foods: 
+    print(item)
+
+for item in foods[1:3]: 
+    print(item)
+    print('Number of letters in',item,':', len(item))    
+'''
+
 #if/elif/else
 '''
 number = 1
